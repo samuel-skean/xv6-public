bd1df3e7ed670364e570e1d47b208af22d1ca990
Samuel Skean <sskean2@uic.edu>
2024-08-29 18:29:31 -0500

    hw1 first attempt.

--- time.c
+++ time.c
@@ -1,5 +1,20 @@
 #include "user.h"
 #include "assert.h"
+#include "types.h"
+
+uint64 current_timestamp(void) {
+    uint32 highOrder, lowOrder;
+    asm ("rdtscp"
+    // outputs to these registers, which are these variables:
+        : "=d" (highOrder), "=a" (lowOrder)
+    // takes no input registers:
+        :
+    // doesn't affect any other registers:
+        :
+    );
+    
+    return (((uint64) highOrder) << 32) | lowOrder;
+}
 
 int main(int argc, char *argv[])
 {
@@ -8,9 +23,14 @@ int main(int argc, char *argv[])
         exit();
     }
 
+    char* program = argv[1];
+    char** program_argv = argv + 1;
+
+    uint64 start = current_timestamp();
+
     if (fork() == 0) {
         // In the child.
-        if (exec(argv[1], argv + 1) == -1) {
+        if (exec(program, program_argv) == -1) {
             printf(2, "exec %s failed. Please ignore the following spurious timing message.\n", argv[1]);
         }
     } else {
@@ -19,7 +39,8 @@ int main(int argc, char *argv[])
             printf(2, "I just spawned a process, but apparently I have no kids. Why?\n");
             exit();
         }
-        printf(1, "The child finished.\n");
+        uint64 end = current_timestamp();
+        printf(1, "%s took %d cycles to run.\n", program, end - start);
     }
     exit();
 }
