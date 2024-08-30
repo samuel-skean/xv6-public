55822cdb15c5bd3b88d8239459aa8c2eccf87d6b
Samuel Skean <sskean2@uic.edu>
2024-08-30 09:22:07 -0500

    More comments!

--- poweroff.c
+++ poweroff.c
@@ -4,6 +4,9 @@
 int
 main(void)
 {
-    // This idea is entirely courtesy of Jacob. He has yet to explain it to me.
+    // This idea is courtesy of Jacob Cohen and the OSDev Wiki:
+    // https://wiki.osdev.org/Shutdown
+    // This only works in qemu, and I'm pretty surprised I can do it in
+    // usermode. But at least I can regain access to my terminals this way!
     outw(0x604, 0x2000);
 }
