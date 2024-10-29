6881330815e57480b14393bd2f5e396b31747a2f
Samuel Skean <sskean2@uic.edu>
2024-10-29 00:57:14 +0000

    This was my original implementation of the lazy mmap that I thought would work.
    Explanations for the bug are in comments starting with BUG EXPLANATION:.

--- sysproc.c
+++ sysproc.c
@@ -168,8 +168,14 @@ handle_pagefault(addr_t va)
   uint fd = NOFILE + 1;
   struct file *f;
   uint mmap_idx;
-  // Find the lazy mmap by going backwards through the list of lazymmaps:
-  for (mmap_idx = proc->lazymmapcount - 1; mmap_idx >= 0; mmap_idx--) {
+  // BUG EXPLANATION: This goes forward through the list of mmaps, and finds the
+  // first one where the address we faulted on is greater than it's start.
+  // Unfortunately, if we are accessing the address of a lazy mmap and we're
+  // faulting, that address is *always* greater than the start address of the
+  // first lazy mmap because the mmaps for a process always increase in user
+  // address through the list of mmaps. So, we always get the first lazy mmap
+  // even if that's not the mmap we're accessing.
+  for (mmap_idx = 0; mmap_idx < proc->lazymmapcount; mmap_idx++) {
     if (va >= proc->lazymmaps[mmap_idx].start) {
       fd = proc->lazymmaps[mmap_idx].fd;
       break;
@@ -194,6 +200,12 @@ handle_pagefault(addr_t va)
   ilock(f->ip);
 
   addr_t uva_page_start = PGROUNDDOWN((addr_t) va);
+  // BUG EXPLANATION: Interestingly, we fail on readi - before we even try to
+  // map in the file at the wrong spot (in the user program, that would be
+  // somewhere already mapped). That's because the offset is calculated from the
+  // user virtual address compared to that start of the original mmap, and that
+  // offset is *always* greater than the size of the file if we've got the wrong
+  // mmap, causing readi to fail.
   if (readi(f->ip, ka, uva_page_start - proc->lazymmaps[mmap_idx].start, PGSIZE) == -1) {
     cprintf("Failed to read\n"); // TODO: Handle short reads, or change comment.
     iunlock(f->ip);
